04f03b37e924916e34faa5d2ab993b150a76d834
tabbema8 <marlon.berchtold@swisscom.com>
2020-11-21 15:32:40 +0100

    add SpringBoot skeleton

---
+++ backend/HighscoreService/src/main/java/com/pacman/gibb/HighscoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pacman.gibb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighscoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighscoreServiceApplication.class, args);
+	}
+
+}
---
+++ backend/HighscoreService/src/test/java/com/pacman/gibb/HighscoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacman.gibb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighscoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
